fd84edcc02a1bec5236d26f389ea7bab1c48940b
helenl122 <helenl122@outlook.com>
2025-05-10 15:38:23 -0700

    Add tab bar

---
+++ app/HelpScreen.tsx
@@ -0,0 +1,9 @@
+// import ScreenHeader from "@/components/ScreenHeader";
+
+const HelpScreen = () => {    
+    // return (
+        // <ScreenHeader headerTitle="Help"/>
+    // );
+}
+
+export default HelpScreen;
---
+++ app/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+// import ScreenHeader from "@/components/ScreenHeader";
+import { View } from "react-native";
+
+const SettingsScreen = () => {
+    return (
+        <View>
+            {/* <ScreenHeader headerTitle="Settings"/> */}
+        </View>
+    );
+}
+
+export default SettingsScreen;
